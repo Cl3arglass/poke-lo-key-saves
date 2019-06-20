70e06419933ecd70c2b65a77a775546fddc57709
Cl3arglass <haltaffer@gmail.com>
2019-06-20 16:10:38 -0700

    add route to get current user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   	post '/login', to: 'sessions#create'
   	resources :users
     resources :pokes
+    get '/get_current_user', to: 'sessions#get_current_user'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
