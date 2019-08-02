6cd7bd36e9eb9036a6d3452bc95e3dfa4b26e676
Cl3arglass <haltaffer@gmail.com>
2019-08-01 17:59:33 -0700

    add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   scope '/api' do
   	post '/login', to: 'sessions#create'
+  	post '/signup', to: 'users#create'
   	resources :users
     resources :pokes
     get '/get_current_user', to: 'sessions#get_current_user'
